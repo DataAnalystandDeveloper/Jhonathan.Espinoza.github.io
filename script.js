741c8e3d90a85154ebd75dd74df7e826c59c61f6
Jhonathan Espinoza <76660732+DataAnalystandDeveloper@users.noreply.github.com>
2024-11-20 11:39:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// Select the text element
+const textElement = document.getElementById('switching-text');
+
+// Define the text options
+const texts = ["Data Analyst", "BI Analyst", "Tableau Analyst"];
+let index = 0;
+
+// Function to switch text
+function switchText() {
+    index = (index + 1) % texts.length; // Cycle through the text options
+    textElement.textContent = texts[index];
+}
+
+// Set an interval to switch text every 2 seconds (2000ms)
+setInterval(switchText, 2000);
